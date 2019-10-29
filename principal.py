c9a0945d777725a1746be1af63f5208afadac602
MariDoes <marielenadominguezespinal@gmail.com>
2019-10-29 07:37:18 -0500

    codificacion

---
+++ principal.py
@@ -0,0 +1,14 @@
+print("Bienvenidos a nuestro sistema matricula"
+"\ndonde aca le ayudaremos a darle infomracion si eres postulante"
+"\no estudiante actual en nuestra universidad")
+print("Selecciona una de las opciones de su preferencia")
+
+def Menu():
+    while True:
+        opcion = int(input("Seleccione una opcion: "))
+        Alumno = "1"
+        Postulante = "2"
+        if (opcion == 1):
+
+    
+        if opcion == 2:
