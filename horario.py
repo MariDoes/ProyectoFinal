b960d9ddb6dd0db0987f1445d9a74ffdce730bc3
Alfonso <alfonso.bricenoc@usil.pe>
2019-11-06 08:50:23 -0500

    act2

--- horario.py
+++ horario.py
@@ -1,5 +1,9 @@
 import os
 os.system("cls")
+#a=[]
+#h=[[1,1000],[2,221]]
+#a.append(h[0])
+#print(a)
 
 #calculo2, fisica2, inferencia estadistica, taller 2, economia, ingles 6
 
@@ -24,6 +28,9 @@ os.system("cls")
 opcionalum = int(input("Seleccione una opcion del menu usilio: "))
 cursos=["c1","f1","poo","t","m","intro"]
 cursos2020=["Calculo 2","Fisica 2","Estadistica", "Taller 2", "Economia", "Ingles 6"]
+diasSemana=["Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"]
+hCalculo2=[]
+
 
 if opcionalum ==2:
     notas=[]
@@ -36,6 +43,51 @@ if opcionalum ==2:
  
     for i in range(6):
         print("Puedes llevar los siguientes cursos "+cursos2020[i])
+        break
+    continuar = input("Desea ir a la eleccion de horarios? S/N: ").lower()
+    if continuar != "s":
+        print("Espere un momento, regresará al menú principal")
+    else:
+        print("A continuación se mostrará la lista de profesores de cada curso que puedas llevar:")
+        print("Cálculo 2: "
+        "\n1. José Reyes"
+        "\n2. Luisa Fernández")
+        a=int(input("Escoja el profesor con el que desea llevar el curso:"))
+        if a==1:
+            hCalculo2.append()
+        else:
+            hCalculo2.append()
+        
+        print("Física 2: "
+        "\n1. Iris García"
+        "\n2. Richard Guanira")
+        b=int(input("Escoja el profesor con el que desea llevar el curso:"))
+
+        print("Estadística: "
+        "\n1. Pedro Montesino"
+        "\n2. Omar Rivas")
+        c=int(input("Escoja el profesor con el que desea llevar el curso:"))
+
+        print("Taller 2:"
+        "\n1. María Cordova"
+        "\n2. Carmen Ramírez")
+        d=int(input("Escoja el profesor con el que desea llevar el curso:"))
+
+        print("Economía:")
+        "\n1. Cielo Tamariz"
+        "\n2. Ricardo Paz")
+        e=int(input("Escoja el profesor con el que desea llevar el curso:"))
+
+        print("Inglés 6:")
+        "\n1. Mariell Muller"
+        "\n2. Rocío Durán")
+        f=int(input("Escoja el profesor con el que desea llevar el curso:"))
+
+        
+
+
+
+
 
 
 
@@ -66,7 +118,7 @@ if opcionalum == 3:
         if notas1[5] >= 10.5:
             cursos = print("Puedes llevar los siguientes cursos "+cursos2020[5])
         break
-    continuar = input("Desea ir a la eleccion de horarios? S/N: ")
+    continuar = input("Desea ir a la eleccion de horarios? S/N: ").lower()
         
 
 
